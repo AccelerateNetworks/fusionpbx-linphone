a5e22d119b94863be6bcfb4f0675fda3dd0dc63a
Kenny Stimson <kenny_stimson@yahoo.com>
2026-06-02 18:07:45 -0700

    Rename $config to avoid name collision in newer FusionPBX versions

--- provision/template.php
+++ provision/template.php
@@ -51,85 +51,85 @@ $parameters['domain_uuid'] = $extension['domain_uuid'];
 $domain_name = $database->select($sql, $parameters, 'column');
 unset($parameters);
 
-$config['misc']['uuid'] = "317971da-65c4-419f-a0ca-69fe26523e2b";
-$config['misc']['transient_provisioning'] = "0";
-$config['misc']['version_check_url_root'] = "https://".$domain_name."/app/linphone";
-$config['misc']['config-uri'] = "https://".$domain_name."/app/linphone/provision/index.php?token=".$_GET['token'];
-$config['misc']['file_transfer_server_url'] = "https://".$domain_name."/app/webtexting/upload-hook.php?token=".$extension['upload_secret'];
+$linphone_config['misc']['uuid'] = "317971da-65c4-419f-a0ca-69fe26523e2b";
+$linphone_config['misc']['transient_provisioning'] = "0";
+$linphone_config['misc']['version_check_url_root'] = "https://".$domain_name."/app/linphone";
+$linphone_config['misc']['config-uri'] = "https://".$domain_name."/app/linphone/provision/index.php?token=".$_GET['token'];
+$linphone_config['misc']['file_transfer_server_url'] = "https://".$domain_name."/app/webtexting/upload-hook.php?token=".$extension['upload_secret'];
 
 
-$config['sip']['verify_server_certs'] = "0";
-$config['sip']['verify_server_cn'] = "0";
-$config['sip']['default_proxy'] = "0";
-$config['sip']['media_encryption'] = "none";
-$config['sip']['lime'] = "0";
+$linphone_config['sip']['verify_server_certs'] = "0";
+$linphone_config['sip']['verify_server_cn'] = "0";
+$linphone_config['sip']['default_proxy'] = "0";
+$linphone_config['sip']['media_encryption'] = "none";
+$linphone_config['sip']['lime'] = "0";
 if($is_mobile) {
-  $config['sip']['use_ipv6'] = "0";
+  $linphone_config['sip']['use_ipv6'] = "0";
 } else {
-  $config['sip']['use_ipv6'] = "0";
+  $linphone_config['sip']['use_ipv6'] = "0";
 }
 
-$config['ui']['exit_on_close'] = "0";
-$config['ui']['logs_enabled'] = "1";
+$linphone_config['ui']['exit_on_close'] = "0";
+$linphone_config['ui']['logs_enabled'] = "1";
 
-$config['proxy_default_values']['avfp'] = "0";
-$config['proxy_default_values']['quality_reporting_collecto'] = "sip:voipmetrics@acceleratenetworks.sip.callpipe.com;transport=tls";
-$config['proxy_default_values']['quality_reporting_enabled'] = "0";
-$config['proxy_default_values']['quality_reporting_interval'] = "100";
-$config['proxy_default_values']['cpim_in_basic_chat_rooms_enabled'] = "1";
+$linphone_config['proxy_default_values']['avfp'] = "0";
+$linphone_config['proxy_default_values']['quality_reporting_collecto'] = "sip:voipmetrics@acceleratenetworks.sip.callpipe.com;transport=tls";
+$linphone_config['proxy_default_values']['quality_reporting_enabled'] = "0";
+$linphone_config['proxy_default_values']['quality_reporting_interval'] = "100";
+$linphone_config['proxy_default_values']['cpim_in_basic_chat_rooms_enabled'] = "1";
 
-$config['auth_info_0']['username'] = $extension['extension'];
-$config['auth_info_0']['passwd'] = $extension['password'];
-$config['auth_info_0']['domain'] = $domain_name;
-$config['auth_info_0']['realm'] = $domain_name;
-$config['auth_info_0']['algorithm'] = "MD5";
+$linphone_config['auth_info_0']['username'] = $extension['extension'];
+$linphone_config['auth_info_0']['passwd'] = $extension['password'];
+$linphone_config['auth_info_0']['domain'] = $domain_name;
+$linphone_config['auth_info_0']['realm'] = $domain_name;
+$linphone_config['auth_info_0']['algorithm'] = "MD5";
 
 $proxy = $domain_name;
 if($is_mobile) {
   $proxy = "flexisip.prod.callpipe.com";
 }
 
-$config['proxy_0']['reg_proxy'] = "<sip:".$proxy.";transport=tls>";
-$config['proxy_0']['reg_route'] = "<sip:".$proxy.";transport=tls>";
-$config['proxy_0']['reg_identity'] = "\"".$extension['effective_caller_id_name']."\" <sips:".$extension['extension']."@".$domain_name.">";
-$config['proxy_0']['realm'] = $domain_name;
+$linphone_config['proxy_0']['reg_proxy'] = "<sip:".$proxy.";transport=tls>";
+$linphone_config['proxy_0']['reg_route'] = "<sip:".$proxy.";transport=tls>";
+$linphone_config['proxy_0']['reg_identity'] = "\"".$extension['effective_caller_id_name']."\" <sips:".$extension['extension']."@".$domain_name.">";
+$linphone_config['proxy_0']['realm'] = $domain_name;
 if($is_mobile) {
-  $config['proxy_0']['reg_expires'] = "604800";
+  $linphone_config['proxy_0']['reg_expires'] = "604800";
 } else {
-  $config['proxy_0']['reg_expires'] = "60";
+  $linphone_config['proxy_0']['reg_expires'] = "60";
 }
-$config['proxy_0']['reg_sendregister'] = "1";
-$config['proxy_0']['publish'] = "1";
-$config['proxy_0']['dial_escape_plus'] = "0";
-$config['proxy_0']['push_notification_allowed'] = "1";
-$config['proxy_0']['cpim_in_basic_chat_rooms_enabled'] = "1";
+$linphone_config['proxy_0']['reg_sendregister'] = "1";
+$linphone_config['proxy_0']['publish'] = "1";
+$linphone_config['proxy_0']['dial_escape_plus'] = "0";
+$linphone_config['proxy_0']['push_notification_allowed'] = "1";
+$linphone_config['proxy_0']['cpim_in_basic_chat_rooms_enabled'] = "1";
 
-$config['nat_policy_0']['protocols'] = "stun";
-$config['nat_policy_0']['stun_server'] = "stun.l.google.com:19302";
+$linphone_config['nat_policy_0']['protocols'] = "stun";
+$linphone_config['nat_policy_0']['stun_server'] = "stun.l.google.com:19302";
 
-$config['nat_policy_default_values']['protocols'] = "stun,ice";
-$config['nat_policy_default_values']['stun_server'] = "stun.l.google.com:19302";
+$linphone_config['nat_policy_default_values']['protocols'] = "stun,ice";
+$linphone_config['nat_policy_default_values']['stun_server'] = "stun.l.google.com:19302";
 
 if(!$is_mobile) { // Linphone Desktop gets a codec list
   $codec_num=0;
   foreach($audio_codecs_enabled as $codec) {
     $section = 'audio_codec_'.$codec_num++;
-    $config[$section]['mime'] = $codec;
-    $config[$section]['enabled'] = 1;
+    $linphone_config[$section]['mime'] = $codec;
+    $linphone_config[$section]['enabled'] = 1;
   }
 
   foreach($audio_codecs_disabled as $codec) {
     $section = 'audio_codec_'.$codec_num++;
-    $config[$section]['mime'] = $codec;
-    $config[$section]['enabled'] = 0;
+    $linphone_config[$section]['mime'] = $codec;
+    $linphone_config[$section]['enabled'] = 0;
   }
 
   $codec_num = 0;
   foreach($video_codecs_enabled as $codec) {
     $section = 'video_codec_'.$codec_num++;
-    $config[$section]['mime'] = $codec;
-    // $config[$section]['rate'] = "90000"; // this was set for VP8
-    $config[$section]['enabled'] = 1;
+    $linphone_config[$section]['mime'] = $codec;
+    // $linphone_config[$section]['rate'] = "90000"; // this was set for VP8
+    $linphone_config[$section]['enabled'] = 1;
   }
 }
 
@@ -137,22 +137,22 @@ if($is_mobile) { // Linphone Desktop gets a codec list
   $codec_num=0;
   foreach($audio_codecs_enabled_mobile as $codec) {
     $section = 'audio_codec_'.$codec_num++;
-    $config[$section]['mime'] = $codec;
-    $config[$section]['enabled'] = 1;
+    $linphone_config[$section]['mime'] = $codec;
+    $linphone_config[$section]['enabled'] = 1;
   }
 
   foreach($audio_codecs_disabled_mobile as $codec) {
     $section = 'audio_codec_'.$codec_num++;
-    $config[$section]['mime'] = $codec;
-    $config[$section]['enabled'] = 0;
+    $linphone_config[$section]['mime'] = $codec;
+    $linphone_config[$section]['enabled'] = 0;
   }
 
   $codec_num = 0;
   foreach($video_codecs_enabled as $codec) {
     $section = 'video_codec_'.$codec_num++;
-    $config[$section]['mime'] = $codec;
-    // $config[$section]['rate'] = "90000"; // this was set for VP8
-    $config[$section]['enabled'] = 1;
+    $linphone_config[$section]['mime'] = $codec;
+    // $linphone_config[$section]['rate'] = "90000"; // this was set for VP8
+    $linphone_config[$section]['enabled'] = 1;
   }
 }
 
@@ -162,20 +162,20 @@ $settings = $database->select($sql, $parameters, 'all');
 unset($parameters);
 foreach($settings as $setting) {
   $key = explode(".", $setting['setting']);
-  $config[$key[0]][$key[1]] = $setting['value'];
+  $linphone_config[$key[0]][$key[1]] = $setting['value'];
 }
 
 // special case values
-if($config['proxy_0']['reg_identity_port']) {
-  $config['proxy_0']['reg_identity'] = "\"".$extension['effective_caller_id_name']."\" <sips:".$extension['extension']."@".$domain_name.":".$config['proxy_0']['reg_identity_port'].">";
-  unset($config['proxy_0']['reg_identity_port']);
+if($linphone_config['proxy_0']['reg_identity_port']) {
+  $linphone_config['proxy_0']['reg_identity'] = "\"".$extension['effective_caller_id_name']."\" <sips:".$extension['extension']."@".$domain_name.":".$linphone_config['proxy_0']['reg_identity_port'].">";
+  unset($linphone_config['proxy_0']['reg_identity_port']);
 }
 
-if($config['proxy_0']['proxy_domain']) {
-  $proxy = $config['proxy_0']['proxy_domain'];
-  unset($config['proxy_0']['proxy_domain']);
-  $config['proxy_0']['reg_proxy'] = "<sip:".$proxy.";transport=tls>";
-  $config['proxy_0']['reg_route'] = "<sip:".$proxy.";transport=tls>";
+if($linphone_config['proxy_0']['proxy_domain']) {
+  $proxy = $linphone_config['proxy_0']['proxy_domain'];
+  unset($linphone_config['proxy_0']['proxy_domain']);
+  $linphone_config['proxy_0']['reg_proxy'] = "<sip:".$proxy.";transport=tls>";
+  $linphone_config['proxy_0']['reg_route'] = "<sip:".$proxy.";transport=tls>";
 }
 
 $sql = "select c.contact_uuid, c.contact_organization, c.contact_name_given, c.contact_name_family, ";
@@ -190,12 +190,12 @@ $database_contacts = $database->select($sql, $parameters, 'all');
 $i = 0;
 foreach ($database_contacts as $contact) {
   $section = "friend_".$i++;
-  $config[$section]['url'] = '"'.$contact['contact_name_given']." ".$contact['contact_name_family']." - ".$contact['contact_organization']." (".$contact['phone_label'].')" <sip:'.$contact['phone_number']."@".$domain_name.">";
-  $config[$section]['pol'] = "accept";
+  $linphone_config[$section]['url'] = '"'.$contact['contact_name_given']." ".$contact['contact_name_family']." - ".$contact['contact_organization']." (".$contact['phone_label'].')" <sip:'.$contact['phone_number']."@".$domain_name.">";
+  $linphone_config[$section]['pol'] = "accept";
   if(strlen($contact['phone_number']) < 8) {
-    $config[$section]['subscribe'] = "1";
+    $linphone_config[$section]['subscribe'] = "1";
   } else {
-    $config[$section]['subscribe'] = "0";
+    $linphone_config[$section]['subscribe'] = "0";
   }
 }
 
@@ -220,7 +220,7 @@ xmlwriter_text($xw, 'http://www.linphone.org/xsds/lpconfig.xsd lpconfig.xsd');
 xmlwriter_end_attribute($xw);
 
 
-foreach($config as $section=>$values) {
+foreach($linphone_config as $section=>$values) {
   xmlwriter_start_element($xw, 'section');
 
   xmlwriter_start_attribute($xw, 'name');
